4d8d4fe20fe0cb2324b118773d6b73cfad9dd9d2
Chiuri254 <joseshiory@gmail.com>
2019-03-18 10:50:19 +0300

    add welcome views

--- rank/views.py
+++ rank/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import  MoringaMerch
-from .serializer import MerchSerializer
+from .serializer import ProfileSerializer, ProjectSerializer
 from .models import Profile, Project, Review
 
 
@@ -20,19 +20,81 @@ def welcome(request):
     projects = Project.objects.order_by('-pub_date')
     return render(request,"welcome.html",{"projects":projects})
 
-# def rank_of_the_day(request):
-#     date = dt.date.today()
-#     html = f'''
-#         <html>
-#             <body>
-#                 <h1> {date.day}-{date.month}-{date.year}</h1>
-#                 </body>
-#                 </html>
-#                 '''
-#     return HttpResponse(html)
 
-# class MerchList(APIView):
-#     def get(self, request, format=None):
-#         all_merch = MoringaMerch.objects.all()
-#         serializers = MerchSerializer(all_merch, many=True)
-#         return Response(serializers.data)
+def project(request, id):
+    if request.user.is_authenticated:
+        user = User.objects.get(username = request.user)
+    project = Project.objects.get(id = id)
+    reviews = Review.objects.filter(project = project)
+    design = reviews.aggregate(Avg('design'))['design__avg']
+    usability = reviews.aggregate(Avg('usability'))['usability__avg']
+    content = reviews.aggregate(Avg('content'))['content__avg']
+    average = reviews.aggregate(Avg('average'))['average__avg']
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.average = (review.design + review.usability + review.content) / 3
+            review.project = project
+            review.user = user
+            review.save()
+        return redirect('project', id)
+    else:
+        form = ReviewForm()
+    return render(request, 'project.html', {'project': project, 'reviews': reviews, 'form': form, 'design': design, 'usability': usability, 'content': content, 'average': average})
+
+def search(request):
+    if 'site' in request.GET and request.GET['site']:
+        search_term = request.GET.get('site')
+        projects = Project.objects.filter(title__icontains = search_term)
+        message = f'{search_term}'
+        return render(request, 'search.html', {'projects': projects, 'message': message})
+        
+    return render(request, 'search.html')
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)
+
+@login_required(login_url='/accounts/login/')
+def upload_site(request):
+    if request.method == 'POST':
+        form = UploadSiteForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = request.user
+            project.save()
+        return redirect('index')
+    else:
+        form = UploadSiteForm()
+
+    return render(request, 'upload.html', {'form': form})
+
+def profile(request, username):
+    user = User.objects.get(username = username)
+    profile = Profile.objects.get(user = user)
+    projects = Project.objects.filter(user = user)
+    return render(request, 'profile.html', {'profile': profile, 'projects': projects})
+
+@login_required(login_url = '/accounts/login/')
+def update_profile(request, id):
+    if request.method == 'POST':
+        profile = Profile.objects.get(id = id)
+        form = UpdateProfile(request.POST or None, request.FILES or None, instance = profile)
+        if form.is_valid():
+            edit = form.save(commit=False)
+            edit.save()
+            return redirect('profile', username = request.user)
+    else:
+        form = UpdateProfile()
+
+    return render(request, 'update_profile.html', {'form': form})
+
